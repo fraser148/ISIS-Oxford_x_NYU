eca8fb3f0de4f63fb41297fc0e426239c9f7340d
fraser148 <fjrennie1@outlook.com>
2021-04-10 12:33:17 +0100

    okay

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.use("/views", express.static(path.join(__dirname, '/views')));
+app.use("/views", express.static(path.resolve(__dirname, '/views')));
 
 // Future Code Goes Here
 
